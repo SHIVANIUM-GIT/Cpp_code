b3dc11f2ee7bc1d98a309e8a1de8fbc3a67329b1
shivanium <shivrathore223@gmail.com>
2023-09-19 16:23:06 +0530

    function_return

--- cpp file/function.cpp
+++ cpp file/function.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
+#include <string>
 
 void HiThere(std::string name, int number)
 {
-    std::cout << "your name is =  " << name << '\n';
-    std::cout << "your age is = " << number << '\n';
+    std::cout << "Your name is: " << name << '\n';
+    std::cout << "Your age is: " << number << '\n';
 }
+
+int Add(int number)
+{
+    return 2 * number;
+}
+
 int main()
 {
     std::string name;
     int number = 0;
-    std::cout << "enter you name = ";
+
+    std::cout << "Enter your name: ";
     std::getline(std::cin >> std::ws, name);
-    std::cout << "enter you age = ";
+
+    std::cout << "Enter your age: ";
     std::cin >> number;
+
+    int doubleAge = Add(number);
+
+    std::cout << "Your age doubled is: " << doubleAge << '\n';
+
     HiThere(name, number);
+
     return 0;
 }
