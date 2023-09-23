dd9914072587c90e08fe1a267b94e1828c8568b7
Shivanium <Shivrathore223@gmail.com>
2023-09-23 14:29:50 +0530

    null pointer

--- cpp file/nullPointer.cpp
+++ cpp file/nullPointer.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 int main()
 {
+    int *pointer = nullptr;
+    int x = 12;
+    pointer = &x;
+    if (pointer == nullptr)
+    {
+        std::cout << "address is not found";
+    }
+    else
+    {
+        std::cout << pointer << '\n';
+        std::cout << *pointer;
+    }
 
     return 0;
 }
